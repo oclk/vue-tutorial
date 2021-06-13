57b22cd1f6dc45ebcf5bacc6f070252badeea7d3
Ömer Çelik <oclk@outlook.com.tr>
2021-06-13 20:33:30 +0300

    Lesson #2 - Application & Component Instances [ 🐣 ]

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Lesson #1 - Introduction [ 🚀 ]
+/*
 const { createApp } = Vue
 
 const App = {
@@ -8,3 +9,46 @@ const App = {
 }
 
 Vue.createApp(App).mount('#app')
+*/
+
+// Lesson #2 - Application & Component Instances [ 🐣 ]
+// Register 'globals'
+const app = Vue.createApp({
+    // options
+})
+app.component('header', Header)
+app.use(Chat)
+app.directive('focusing', FocusingName)
+
+// Chaining
+Vue.createApp({})
+    .component('header', Header)
+    .use(Chat)
+    .directive('focusing', FocusingName)
+
+// Component Instance Properties
+Vue.createApp({
+    data: () => ({
+        message: '',
+        flag: false,
+    })
+})
+
+// Lifecycle Hooks
+Vue.createApp({
+    data: () => ({
+        counter: 0
+    }),
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        console.log('created')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    updated() {
+        console.log('updated' + ' Counter: ' + this.counter)
+    },
+}).mount('#app')
